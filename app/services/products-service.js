f6fa6ec49323041746c5d16dbe590c8226466135
Elena Poelman <elena.poelman@redpencil.io>
2024-10-01 15:27:24 +0200

    formatting

--- app/services/products-service.js
+++ app/services/products-service.js
@@ -5,16 +5,29 @@ export default class ProductsService extends Service {
 
   async fetchSuggestion(decisionUri) {
     return {
-             "title":  "Tegemoetkoming in de kosten voor kadervorming voor animatoren in het jeugdwerk",
-             "description":  "Vanaf 16 jaar kan een jongere animator in het jeugdwerk worden. Hiervoor kan hij kadervorming volgen.\n\nJe krijgt een tegemoetkoming voor de kosten van het volgen van de kadervorming.",
-             "costs":  [],
-             "requirements":  [{"title":  "Minimumleeftijd", "description":  "Minimumleeftijd: 15 jaar"},
-               {"title":  "Plaats", "description":  "Gedomicilieerd in Sint-Niklaas"},
-               {"title":  "Maximumleeftijd", "description":  "Maximumleeftijd: 30 jaar"},
-               {"title":  "Bewijs", "description":  "Een bewijs van deelname van minstens vier aaneensluitende vormingsuren kunnen worden voorgelegd."}
-             ],
-             "procedures":  [{"title":  "Aanvraag procedure", "description":  "Nadat je hebt deelgenomen, vul je het aanvraagformulier in.\n\nJe kan het aanvraagformulier online invullen."}]
-           };
+      title:
+        'Tegemoetkoming in de kosten voor kadervorming voor animatoren in het jeugdwerk',
+      description:
+        'Vanaf 16 jaar kan een jongere animator in het jeugdwerk worden. Hiervoor kan hij kadervorming volgen.\n\nJe krijgt een tegemoetkoming voor de kosten van het volgen van de kadervorming.',
+      costs: [],
+      requirements: [
+        { title: 'Minimumleeftijd', description: 'Minimumleeftijd: 15 jaar' },
+        { title: 'Plaats', description: 'Gedomicilieerd in Sint-Niklaas' },
+        { title: 'Maximumleeftijd', description: 'Maximumleeftijd: 30 jaar' },
+        {
+          title: 'Bewijs',
+          description:
+            'Een bewijs van deelname van minstens vier aaneensluitende vormingsuren kunnen worden voorgelegd.',
+        },
+      ],
+      procedures: [
+        {
+          title: 'Aanvraag procedure',
+          description:
+            'Nadat je hebt deelgenomen, vul je het aanvraagformulier in.\n\nJe kan het aanvraagformulier online invullen.',
+        },
+      ],
+    };
   }
   fetchProducts = task(async () => {
     return this.products;
