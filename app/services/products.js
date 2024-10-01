f21b3676e49b1852ffeebd091bb9dc733405b51c
pj-cegeka <pieterjan.livens@cegeka.com>
2024-10-01 14:51:18 +0200

    Feat: Fill in meaningful mock data

--- app/services/products.js
+++ app/services/products.js
@@ -5,14 +5,16 @@ export default class ProductsService extends Service {
 
   async fetchSuggestion(decisionUri) {
     return {
-      title: 'Sample title',
-      description: 'Sample description',
-      costs: [{ title: 'Cost 1', description: 'Cost 1 desc' }],
-      procedures: [{ title: 'Procedure 1', description: 'Procedure 1 desc' }],
-      requirements: [
-        { title: 'Requirement 1', description: 'Requirement 1 desc' },
-      ],
-    };
+             "title":  "Tegemoetkoming in de kosten voor kadervorming voor animatoren in het jeugdwerk",
+             "description":  "Vanaf 16 jaar kan een jongere animator in het jeugdwerk worden. Hiervoor kan hij kadervorming volgen.\n\nJe krijgt een tegemoetkoming voor de kosten van het volgen van de kadervorming.",
+             "costs":  [],
+             "requirements":  [{"title":  "Minimumleeftijd", "description":  "Minimumleeftijd: 15 jaar"},
+               {"title":  "Plaats", "description":  "Gedomicilieerd in Sint-Niklaas"},
+               {"title":  "Maximumleeftijd", "description":  "Maximumleeftijd: 30 jaar"},
+               {"title":  "Bewijs", "description":  "Een bewijs van deelname van minstens vier aaneensluitende vormingsuren kunnen worden voorgelegd."}
+             ],
+             "procedures":  [{"title":  "Aanvraag procedure", "description":  "Nadat je hebt deelgenomen, vul je het aanvraagformulier in.\n\nJe kan het aanvraagformulier online invullen."}]
+           };
   }
   fetchProducts = task(async () => {
     return this.products;
