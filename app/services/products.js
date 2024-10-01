301e4f13d0cbdafe839985412dead36e692d7880
Elena Poelman <elena.poelman@redpencil.io>
2024-10-01 13:13:22 +0200

    update products-service to support adding a product (mock)

--- app/services/products.js
+++ app/services/products.js
@@ -1,10 +1,16 @@
 import Service from '@ember/service';
 import { task } from 'ember-concurrency';
 export default class ProductsService extends Service {
+  products = [];
+
   fetchSuggestion = task(async (decisionUri) => {
     return {};
   });
   fetchProducts = task(async () => {
-    return [];
+    return this.products;
+  });
+
+  addProduct = task(async (product) => {
+    this.products.push(product);
   });
 }
