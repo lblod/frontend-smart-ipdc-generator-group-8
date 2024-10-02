fc3d683647b05057a397ac03f2e701980c91fd8a
Elena Poelman <elena.poelman@redpencil.io>
2024-10-02 12:43:10 +0200

    connect frontend to AI service

--- app/services/products-service.js
+++ app/services/products-service.js
@@ -6,78 +6,52 @@ export default class ProductsService extends Service {
   @service store;
 
   async fetchSuggestion(submissionUri) {
-    const response = {
-      name: 'Tegemoetkoming in de kosten voor kadervorming voor animatoren in het jeugdwerk',
-      description:
-        'Vanaf 16 jaar kan een jongere animator in het jeugdwerk worden. Hiervoor kan hij kadervorming volgen.\n\nJe krijgt een tegemoetkoming voor de kosten van het volgen van de kadervorming.',
-      procedure: [
-        {
-          name: 'Aanvraag procedure',
-          description:
-            'Nadat je hebt deelgenomen, vul je het aanvraagformulier in.\n\nJe kan het aanvraagformulier online invullen.',
+    const response = await fetch(
+      'http://smart-ipdc-generator.hackathon-ai-8.s.redhost.be:8080/decision',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
         },
-      ],
-      cost: [],
-      condition: [
-        { name: 'Minimumleeftijd', description: 'Minimumleeftijd: 15 jaar' },
-        { name: 'Plaats', description: 'Gedomicilieerd in Sint-Niklaas' },
-        { name: 'Maximumleeftijd', description: 'Maximumleeftijd: 30 jaar' },
-        {
-          name: 'Bewijs',
-          description:
-            'Een bewijs van deelname van minstens vier aaneensluitende vormingsuren kunnen worden voorgelegd.',
-        },
-      ],
-      entry_theme: ['Cultuur, Sport en Vrije Tijd'],
-      entry_type: 'Financieel voordeel',
-      entry_doelgroep: ['Burger', 'Organisatie'],
-      bevoegde_bestuursniveau: ['Provinciale overheid'],
-      uitvoerende_bestuursniveau: ['Lokale overheid'],
-    };
-    const themes = await Promise.all(
-      response.entry_theme.map((t) => this.fetchConceptByLabel(t))
+        body: JSON.stringify({
+          uri: submissionUri,
+        }),
+      }
     );
-    const type = await this.fetchConceptByLabel(response.entry_type);
+    console.log('Response: ', response);
+    const data = (await response.json()).entry;
+    const themes = await Promise.all(
+      data.entry_theme.map((t) => this.fetchConceptByLabel(t))
+    );
+    const type = await this.fetchConceptByLabel(data.entry_type);
     const doelgroepen = (
       await Promise.all(
-        response.entry_doelgroep.map((d) => this.fetchConceptByLabel(d))
+        data.entry_doelgroep.map((d) => this.fetchConceptByLabel(d))
       )
     ).filter(Boolean);
     const bevoegdeBestuursniveaus = (
       await Promise.all(
-        response.bevoegde_bestuursniveau.map((b) => this.fetchConceptByLabel(b))
+        data.bevoegde_bestuursniveau.map((b) => this.fetchConceptByLabel(b))
       )
     ).filter(Boolean);
     const uitvoerendeBestuursniveaus = (
       await Promise.all(
-        response.uitvoerende_bestuursniveau.map((b) =>
-          this.fetchConceptByLabel(b)
-        )
+        data.uitvoerende_bestuursniveau.map((b) => this.fetchConceptByLabel(b))
       )
     ).filter(Boolean);
     return {
-      name: response.name,
-      description: response.description,
-      procedures: response.procedure.map((p) => tracked(p)),
-      costs: response.cost.map((c) => tracked(c)),
-      requirements: response.condition.map((c) => tracked(c)),
+      name: data.name,
+      description: data.description,
+      procedures: data.procedure.map((p) => tracked(p)),
+      costs: data.cost.map((c) => tracked(c)),
+      requirements: data.condition.map((c) => tracked(c)),
       themes,
       type,
       doelgroepen,
       bevoegdeBestuursniveaus,
       uitvoerendeBestuursniveaus,
     };
-    // const response = await fetch('/decision-consumer/decision', {
-    //   method: 'POST',
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     uri: submissionUri,
-    //   }),
-    // });
-    // const data = await response.json();
     // return data;
   }
 
